c253419aafae0151b39b9f5df03dda7a2b8e0ffc
krsnananda <krsnananda@outlook.com>
2019-12-28 18:31:00 -0300

    - Implements responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.scss';
 import Menu from './pages/Menu';
+import Navbar from "./pages/Navbar";
 
-function App() {
-  return (
-    <div className="Aurum">
+import GlobalStyle from './styles/Global';
+
+class App extends Component {
+  state = {
+    navbarOpen: false
+  }
+
+  handleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
+  }
+
+  render() {
+
+    return (
+      <>
+        <Navbar 
+          navbarState={this.state.navbarOpen} 
+          handleNavbar={this.handleNavbar}
+        />
+        <GlobalStyle />
+        <div className="Aurum">
       <header className="Aurum-header">
         <Menu />
       </header>
       <body className="Aurum-body"> Teste </body>
     </div>
-  );
+      </>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="Aurum">
+//       <header className="Aurum-header">
+//         <Menu />
+//       </header>
+//       <body className="Aurum-body"> Teste </body>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -3,20 +3,22 @@ import book from '../assets/book.svg';
 import chart from '../assets/chart.svg';
 import gear from '../assets/gear.svg';
 import play from '../assets/play.svg';
-// import plus from '../assets/plus.svg';
 import stack from '../assets/stack.svg';
 
 export default class Menu extends React.Component {
     
     render() {
         return (
-            <div className="header-menu">
+            <section className="headerMenu">
+                <div className="allCategories">
+                    <img src={stack} className="stack-icon" alt="stack" />
+                </div> 
+
                 <img src={book} className="book-icon" alt="book" />
                 <img src={chart} className="chart-icon" alt="chart" />
-                <img src={gear} className="gear-icon" alt="gear" />
                 <img src={play} className="play-icon" alt="play" />
-                <img src={stack} className="stack-icon" alt="stack" />
-            </div>
+                <img src={gear} className="gear-icon" alt="gear" />
+            </section>
         )
     }
 }
